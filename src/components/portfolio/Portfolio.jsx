ad124de5d820ab2ed6681e2361618396110de61d
Dibeisi <tomekszyluk@gmail.com>
2023-04-25 16:08:33 +0200

    add dashboard

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
+import IMG1 from '../../assets/portfolio5.jpg'
+import IMG2 from '../../assets/portfolio1.jpg'
+import IMG3 from '../../assets/portfolio2.jpg'
+import IMG4 from '../../assets/portfolio3.jpg'
+import IMG5 from '../../assets/portfolio4.jpg'
 
 const data = [
   {
     id: 1,
     image: IMG1,
+    title: 'Dashboard',
+    github: 'https://github.com/Dibeisi/Admin-Dashboard',
+    demo: 'https://dibeisi.github.io/Admin-Dashboard/'
+  },
+  {
+    id: 2,
+    image: IMG2,
     title: 'Converter °C to °F',
     github: 'https://github.com/Dibeisi/Converter',
     demo: 'https://s027q6.csb.app/'
   },
   {
-    id: 2,
-    image: IMG2,
+    id: 3,
+    image: IMG3,
     title: 'TodoList App',
     github: 'https://github.com/Dibeisi/ToDoList',
     demo: 'https://k4swk9.csb.app/'
   },
   {
-    id: 3,
-    image: IMG3,
+    id: 4,
+    image: IMG4,
     title: 'PasswordValidator',
     github: 'https://github.com/Dibeisi/PasswordValidator',
     demo: 'https://eo3tgp.csb.app/'
   },
   {
-    id: 4,
-    image: IMG4,
+    id: 5,
+    image: IMG5,
     title: 'WeatherApp',
     github: 'https://github.com/Dibeisi/WeatherApp',
     demo: 'https://udw2hn.csb.app/'
